1fe415c8b4fc47b18306357ace75d0b10aa09afd
Tash Cruz <sebastiantashika@gmail.com>
2022-11-09 15:31:00 +0900

    updated Restaurant model's validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 class Restaurant < ApplicationRecord
   CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  has_many :reviews
+
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true
+  validates :category, inclusion: { in: CATEGORY }
 end
