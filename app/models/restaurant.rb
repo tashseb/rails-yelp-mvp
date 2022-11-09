292e526d62cb47f99fa671a034eefa28c84199f4
Tash Cruz <sebastiantashika@gmail.com>
2022-11-09 15:13:18 +0900

    added validation on review model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :name, presence: true
   validates :address, presence: true
-  validates :phone_number, presence: true
+  validates :category, presence: true
 end
